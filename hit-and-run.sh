76667a91617503593e1006455560a82080e2c7b6
Connor McDonald <connor_mcdonald@intuit.com>
2020-03-06 12:03:28 -0600

    hide outputs

--- hit-and-run.sh
+++ hit-and-run.sh
@@ -1,4 +1,4 @@
-cd ~
-git clone https://github.com/clmcdonald/goat-desktop.git
-osascript ~/goat-desktop/desktop.applescript
-rm -rf goat-desktop
+cd ~ > /dev/null 2>&1
+git clone https://github.com/clmcdonald/goat-desktop.git > /dev/null 2>&1
+osascript ~/goat-desktop/desktop.applescript > /dev/null 2>&1
+rm -rf goat-desktop > /dev/null 2>&1
